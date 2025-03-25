71ae8ad9ecbb4ec2a6070ca785da67c98addaba2
andrewwang16 <89804002+andrewwang16@users.noreply.github.com>
2025-03-25 12:42:18 -0500

    Freeze tests fixed

--- src/tests/BankAccountTests.java
+++ src/tests/BankAccountTests.java
@@ -2,6 +2,7 @@ package tests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 
 import org.junit.jupiter.api.Test;
