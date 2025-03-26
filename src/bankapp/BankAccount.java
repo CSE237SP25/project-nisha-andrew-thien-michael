5554689b88c3c2f556243500a9d8739f893b65e2
andrewwang16 <89804002+andrewwang16@users.noreply.github.com>
2025-03-25 19:52:11 -0500

    Account freeze done and tested

--- src/bankapp/BankAccount.java
+++ src/bankapp/BankAccount.java
@@ -3,26 +3,44 @@ package bankapp;
 public class BankAccount {
 
 	private double balance;
+	private boolean isFrozen;
 	
 	public BankAccount() {
 		this.balance = 0;
+		this.isFrozen = false;
 	}
 	
 	public void deposit(double amount) {
-		if(amount < 0) {
-			throw new IllegalArgumentException();
+		if(!this.isFrozen) {
+			if(amount < 0) {
+				throw new IllegalArgumentException();
+			}
+			this.balance += amount;
 		}
-		this.balance += amount;
 	}
 
 	public void withdraw(double amount){
-	        if(amount < 0 || amount > this.balance()){
-	        	throw new IllegalArgumentException();
-	        }
-	        this.balance -= amount;        
+		if(!this.isFrozen) {
+			if(amount < 0 || amount > this.balance){
+		        throw new IllegalArgumentException();
+		    }
+		    this.balance -= amount;
+		}
+	}
+	
+	public void freeze() {
+		this.isFrozen = true;
+	}
+	
+	public void unfreeze() {
+		this.isFrozen = false;
 	}
 	
 	public double getCurrentBalance() {
 		return this.balance;
+	}
+	
+	public boolean getFrozenStatus() {
+		return this.isFrozen;
 	}
 }
